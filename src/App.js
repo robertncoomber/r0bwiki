cd71746232d7dd13ef9633d806fd92869c8f027c
robertncoomber <robertnc@gmail.com>
2022-05-07 04:41:18 -0700

    savepoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import "./Ascii";
+import Ascii from "./Ascii";
+import HomePage from "./HomePage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <HomePage />;
+            <Ascii />;
+            <a href="www.google.com" className="title">
+                4th Floor - my audio company
+            </a>
+            <p className="otherText">
+                I Solve audio problems for immersive applications.
+            </p>
+            ;
+        </div>
+    );
 }
 
 export default App;
---
+++ src/HomePage.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+
+export default function HomePage() {
+    const [time, setTime] = useState(getTime());
+
+    function getDate() {
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, "0");
+        var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+        var yyyy = today.getFullYear();
+
+        today = mm + "/" + dd + "/" + yyyy;
+        return today;
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => setTime(getTime(), 1000));
+        return () => {};
+    }, []);
+
+    function getTime() {
+        var today = new Date();
+        var time =
+            today.getHours() +
+            ":" +
+            today.getMinutes() +
+            ":" +
+            today.getSeconds();
+        return time;
+    }
+
+    return (
+        <div>
+            <h1 className="title">r0b.wiki - ROBERT COOMBER - {getTime()}</h1>
+        </div>
+    );
+}
