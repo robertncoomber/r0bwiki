4c45f37016aecc63ed30ec19e0860a2a29e9d296
Robert Coomber <robertnc@gmail.com>
2022-05-08 00:17:26 -0700

    updated styles

--- src/BottomButtons.js
+++ src/BottomButtons.js
@@ -2,8 +2,9 @@ export default function BottomButtons()
 {
     return (
         <div>
-            <a className="buttonsText" href="www.4th-floor.com">4th Floor</a><br/>
-            <a className="buttonsText" href="twitter.com/r0bwiki">Twitter</a><br/>
+            <p className="secondaryText">VR & Music</p>
+            <a className="buttonsText" href="www.4th-floor.com">4th Floor</a>
+            <a className="buttonsText" href="twitter.com/r0bwiki">Twitter</a>
             <a className="buttonsText" href="www.github.com/robertncoomber">Github</a>
         </div>
     )
