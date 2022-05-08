14f0c87fdddc7e524f09f4624ba5b0150efeacdd
Robert Coomber <robertnc@gmail.com>
2022-05-08 00:04:51 -0700

    updated buttons

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import "./styles.css";
 import "./Ascii";
 import Ascii from "./Ascii";
 import HomePage from "./HomePage";
+import BottomButtons from "./BottomButtons";
 
 function App() {
     return (
         <div>
             <HomePage />;
             <Ascii />;
-            <a href="www.google.com" className="title">
-                4th Floor - my audio company
-            </a>
-            <p className="otherText">
-                I Solve audio problems for immersive applications.
-            </p>
+            <BottomButtons/>;
         </div>
     );
 }
---
+++ src/BottomButtons.js
@@ -0,0 +1,10 @@
+export default function BottomButtons()
+{
+    return (
+        <div>
+            <a className="buttonsText" href="www.4th-floor.com">4th Floor</a><br/>
+            <a className="buttonsText" href="twitter.com/r0bwiki">Twitter</a><br/>
+            <a className="buttonsText" href="www.github.com/robertncoomber">Github</a>
+        </div>
+    )
+}
