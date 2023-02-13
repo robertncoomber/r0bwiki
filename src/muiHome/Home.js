58b7f2a9f848da1bf7cb609ac0c38d137da88b47
Robert Coomber <robertnc@gmail.com>
2023-02-13 15:29:29 -0800

    fixed gramar

--- src/muiHome/Home.js
+++ src/muiHome/Home.js
@@ -20,7 +20,7 @@ const Home = () => {
             </Box>
             <Typography variant="body1" component="div" sx={{paddingTop: 6}} gutterBottom flexGrow={1}>
                 I am the technical co-founder of <Link sx={{cursor: "pointer"}} onClick={() => {window.open("https://4th-floor.com")}}>4th Floor</Link>, an immersive audio company. I currently live in Los Angeles, CA. I am passionate 
-about advancing consumer xr technology through audio and software
+about advancing consumer xr technology through audio and software.
             </Typography>
             <Typography variant="h6" component="div" sx={{paddingTop: 4}} gutterBottom flexGrow={1}>
                 Projects
